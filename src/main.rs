8d2c68559886d1757263c840c07020384e860923
Justin Heyes-Jones <justinhj@gmail.com>
2021-12-07 23:32:53 -0800

    pre refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// for r
+// From https://adventofcode.com/2021/day/3
 
 const INPUT: &str = include_str!("../inputs/day3.txt");
 
@@ -16,7 +16,7 @@ const SAMPLE: &str = "00100
 01010";
 
 // Given the vector of words calculate the total ones in each column
-fn totals(words: Vec<Vec<u8>>) -> Vec<u32> {
+fn totals(words: &Vec<Vec<u8>>) -> Vec<u32> {
     let len = words[0].len();
     let counts = vec![0u32; len];
 
@@ -42,18 +42,17 @@ fn binary_digit_list_to_number(input: Vec<u32>) -> u32 {
     acc
 }
 
-fn solve(input: &str) -> u32 {
+fn solve_part_1(input: &str) -> u32 {
 
     let words: Vec<Vec<u8>> = input
         .lines()
         .map(|s| String::into_bytes(s.to_string()))
         .collect();
-    let word_length = words[0].len();
     let half_num_words = words.len() / 2;
 
     // println!("{} words with length {}", words.len(), word_length);
 
-    let totals = totals(words);
+    let totals = totals(&words);
 
     // println!("{:?}", totals);
 
@@ -77,12 +76,70 @@ fn solve(input: &str) -> u32 {
     one_winners_count * zero_winners_count
 }
 
+// Filter words including only those with the given valur at the specified bit position
+fn filter_words(words: Vec<Vec<u8>>, target: &u8, bit: usize) -> Vec<Vec<u8>> {
+
+    words.into_iter()
+        .filter(|word| word[bit] == *target)
+        .collect::<Vec<Vec<u8>>>()
+}
+
+fn solve_part_2(input: &str) -> u32 {
+
+    let words: Vec<Vec<u8>> = input
+        .lines()
+        .map(|s| String::into_bytes(s.to_string()))
+        .collect();
+    let half_num_words = words.len() / 2;
+    let word_length = words[0].len();
+
+    println!("num words {:?}", words.len());
+
+    let totals = totals(&words);
+    println!("totals {:?}", totals);
+
+    // Generate the winners (1,0) for each bit position
+    // If it is a draw then 1 wins
+
+    let winners: Vec<u8> = totals.iter().map (
+        |total| {
+            if *total as usize >= half_num_words {
+                '1' as u8
+            } else {
+                '0' as u8
+            }
+        }
+    ).collect();
+
+    println!("winners {:?}", winners);
+
+    // probably can negate winners to get losers
+    
+    // now eliminate non winners from each bit position until only word is left
+    let mut candidates = words.clone();
+
+    for (bit, winner) in winners.iter().rev().enumerate() {
+        candidates = filter_words(candidates, &winner, bit);
+        println!("{:?}", candidates);
+        if candidates.len() == 1 {
+            break;
+        }
+    }
+
+    println!("winrar {:?}", candidates[0]);
+    let winners: Vec<u32> = candidates[0].iter().map(|n| *n as u32 - '0' as u32).collect(); 
+    let winners_number = binary_digit_list_to_number(winners);
+
+    winners_number
+}
 
 fn main() {
 
-   println!("{}", solve(SAMPLE)); 
-   println!("{}", solve(INPUT)); 
+   println!("{}", solve_part_1(SAMPLE)); 
+   println!("{}", solve_part_1(INPUT)); 
 
 
+   println!("{}", solve_part_2(SAMPLE)); 
+   //println!("{}", solve_part_2(INPUT)); 
 }
 
