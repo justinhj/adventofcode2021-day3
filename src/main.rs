490905c14b52b8e559d5501e044a8001d96285a7
Justin Heyes-Jones <justinhj@gmail.com>
2021-12-08 23:42:48 -0800

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // From https://adventofcode.com/2021/day/3
 
+// TODO some generic stuff needed for binary to digit code
+
 const INPUT: &str = include_str!("../inputs/day3.txt");
 
 const SAMPLE: &str = "00100
@@ -29,7 +31,22 @@ fn totals(words: &Vec<Vec<u8>>) -> Vec<u32> {
         })
 }
 
-fn binary_digit_list_to_number(input: Vec<u32>) -> u32 {
+fn binary_digit_list_to_number(input: &Vec<u32>) -> u32 {
+    let mut acc: u32 = 0;
+    for (i,n) in input.iter().rev().enumerate() {
+        acc += if *n == 1 {
+            2u32.pow(i as u32)
+        }
+        else {
+            0
+        }
+    }
+    acc
+}
+
+// TODO make these two functions generic and work for any number type
+
+fn binary_digit_list_to_number_u8(input: &Vec<u8>) -> u32 {
     let mut acc: u32 = 0;
     for (i,n) in input.iter().rev().enumerate() {
         acc += if *n == 1 {
@@ -73,18 +90,43 @@ fn solve_part_1(input: &str) -> u32 {
   
     let zero_winners: Vec<u32> = one_winners.iter().map(|n| if *n == 0 {1} else {0}).collect();
 
-    let one_winners_count = binary_digit_list_to_number(one_winners);
-    let zero_winners_count = binary_digit_list_to_number(zero_winners);
+    let one_winners_count = binary_digit_list_to_number(&one_winners);
+    let zero_winners_count = binary_digit_list_to_number(&zero_winners);
 
     one_winners_count * zero_winners_count
 }
 
 // Filter words including only those with the given valur at the specified bit position
-fn filter_words(words: Vec<Vec<u8>>, target: &u8, bit: usize) -> Vec<Vec<u8>> {
+fn filter_words(words: Vec<Vec<u8>>, target: u8, bit: usize) -> Vec<Vec<u8>> {
 
     words.into_iter()
-        .filter(|word| word[bit] == *target)
+        .filter(|word| word[bit] == target)
         .collect::<Vec<Vec<u8>>>()
+}
+
+fn get_winner(words: &Vec<Vec<u8>>, index: usize, most_common: bool) -> u8 {
+    let col: Vec<u8> = words
+        .iter()
+        .map(|word| word[index])
+        .collect();
+    let ones: usize = col.iter().fold(0,|a,n| a + *n as usize);
+    let num_words = words.len();
+
+    let zeros = num_words - ones;
+
+    if most_common {
+        if ones >= zeros {
+            1
+        } else {
+            0
+        }
+    } else {
+        if ones >= zeros {
+            0
+        } else {
+            1
+        }
+    }
 }
 
 fn solve_part_2(input: &str) -> u32 {
@@ -96,56 +138,60 @@ fn solve_part_2(input: &str) -> u32 {
             bs.iter().map(|n| *n - '0' as u8).collect()    
         })
         .collect();
-    let half_num_words = words.len() / 2;
 
-    println!("words {:?}", words);
-    println!("num words {:?}", words.len());
+    let word_size = words[0].len();
 
-    let totals = totals(&words);
-    println!("totals {:?}", totals);
+    // println!("words {:?}", words);
+    println!("num words {:?} size {:?}", words.len(), word_size);
 
-    // Generate the winners (1,0) for each bit position
-    // If it is a draw then 1 wins
+    // Take a mutable copy of the words
+    // For each bit from word_size-1 to 0
+    //   get winning bit for the bit index 
+    //   filter the word list for matches 
+    // convert to number 
+    // profit 
 
-    let winners: Vec<u8> = totals.iter().map (
-        |total| {
-            if *total as usize >= half_num_words {
-                1
-            } else {
-                0
-            }
-        }
-    ).collect();
+    // TODO this could be more DRY
 
-    println!("winners {:?}", winners);
-
-    // probably can negate winners to get losers
-    
-    // now eliminate non winners from each bit position until only word is left
     let mut candidates = words.clone();
 
-    for (bit, winner) in winners.iter().enumerate() {
-        candidates = filter_words(candidates, &winner, bit);
-        println!("{:?} {:?} {:?}", candidates, bit, winner);
+    for bit in 0..word_size {
+        let winner = get_winner(&candidates, bit, true);
+        // println!("{:?} bit {:?} winner {:?}", candidates, bit, winner);
+        candidates = filter_words(candidates, winner, bit);
         if candidates.len() == 1 {
             break;
         }
     }
 
-    println!("winrar {:?}", candidates[0]);
-    let winners: Vec<u32> = candidates[0].iter().map(|n| *n as u32 - '0' as u32).collect(); 
-    let winners_number = binary_digit_list_to_number(winners);
+    let oxygen = binary_digit_list_to_number_u8(&candidates[0]);
 
-    winners_number
+    candidates = words.clone();
+
+    for bit in 0..word_size {
+        let winner = get_winner(&candidates, bit, false);
+        // println!("{:?} bit {:?} winner {:?}", candidates, bit, winner);
+        candidates = filter_words(candidates, winner, bit);
+        if candidates.len() == 1 {
+            break;
+        }
+    }
+
+    let scrubber = binary_digit_list_to_number_u8(&candidates[0]);
+
+    println!("{:?} x {:?}", oxygen, scrubber);
+
+
+   oxygen * scrubber 
 }
 
 fn main() {
 
    println!("{}", solve_part_1(SAMPLE)); 
-   println!("{}", solve_part_1(INPUT)); 
+   println!("{}", solve_part_1(INPUT)); // 3320834
 
 
    println!("{}", solve_part_2(SAMPLE)); 
-   //println!("{}", solve_part_2(INPUT)); 
+   println!("{}", solve_part_2(INPUT));  // 4481199
 }
 
